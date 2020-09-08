b91a937fb3622a289a01ea5d96afdc5c17313ddb
DESKTOP-OF2EO8K\urano <urano@nstg.co.jp>
2020-09-08 17:39:24 +0900

    ログイン画面作成途中

--- src/functions/index.php
+++ src/functions/index.php
@@ -1,1 +1,21 @@
-<?php phpinfo();
+<?php
+$title = 'ToDo';
+require_once('./common/meta.php');
+?>
+<html>
+
+<body>
+    <div class="wrapper login-box">
+        <div class="login-form">
+            <form action="#" method="post">
+                <input type="email" name="email" placeholder="Email"><br>
+                <input type="password" name="password" placeholder="Password"><br>
+                <input type="submit" name="login" value="ログイン">
+            </form>
+        </div>
+        <p><a href="">パスワードを忘れた場合</a></p>
+        <p><a href="">新規アカウント登録</a></p>
+    </div>
+</body>
+
+</html>
