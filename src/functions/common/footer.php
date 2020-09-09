011957bf736aeb39dc1ad47ca59b57d07f0e1e01
DESKTOP-OF2EO8K\urano <urano@nstg.co.jp>
2020-09-09 13:56:56 +0900

    ヘッダー、フッター作成

---
+++ src/functions/common/footer.php
@@ -0,0 +1,4 @@
+<footer class="center footer">
+    <small>&copy;2020 urano</small>
+</footer>
+<script src="./../../js/script.js"></script>
---
+++ src/functions/common/header.php
@@ -0,0 +1,3 @@
+<header class="header">
+    <h2><img src="./../img/Y_logo.png" alt="">ToDo</h2>
+</header>
