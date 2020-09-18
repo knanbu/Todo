93a37019ac171ce5cfe684257ecd07b8aec094ae
DESKTOP-OF2EO8K\urano <urano@nstg.co.jp>
2020-09-18 11:30:42 +0900

    行の変更

--- src/functions/account/regist.php
+++ src/functions/account/regist.php
@@ -7,10 +7,10 @@ require('lib/validate.class.php');
 $pdo = new Database(); //データベース接続
 $session = new Session();
 $acc = new Account($pdo);
+$err = new Validate($pdo);
 
 unset($_POST['regist']); //必要のない情報を破棄
 $data = $_POST; //ログイン画面で入力されたデータを格納
-$err = new Validate($pdo);
 $err_array = $err->err_check($data);
 if (empty($err_array)) { //エラーがなければアカウント登録開始
     $result = $err->isset_email($data['email']);
