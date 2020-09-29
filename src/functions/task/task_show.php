31feda3edbc528226c6571cecb58f3bdbd87ae1b
DESKTOP-OF2EO8K\urano <urano@nstg.co.jp>
2020-09-29 17:24:28 +0900

    途中

--- src/functions/task/task_show.php
+++ src/functions/task/task_show.php
@@ -9,14 +9,13 @@ $member_id = $_SESSION['member_id']; //アカウント情報
 $task_list = $task->getTaskList($member_id); //各アカウントのタスク取得
 $category_list = $task->getCategoryList($member_id); //各アカウントのカテゴリー覧取得
 
-if ($_GET['task_id']) {//タスクの詳細
+if ($_GET['task_id']) { //タスクの詳細
     $task_id = $_GET['task_id'];
     $task_info = $task->getTask($task_id);
     $category_name = $task->getCategoryName($task_info[0]['category_id']);
 }
 
-if ($_GET['category_id']) {//カテゴリーの編集
-    $category_id=$_GET['category_id'];
-    $category_name=$task->getCategoryName($category_id);
+if ($_GET['category_id']) { //カテゴリーの編集
+    $category_id = $_GET['category_id'];
+    $category_name = $task->getCategoryName($category_id);
 }
-
