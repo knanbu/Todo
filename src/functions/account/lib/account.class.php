bf925c8dffc78a727851c4a8a0011fcc1c433f71
DESKTOP-OF2EO8K\urano <urano@nstg.co.jp>
2020-09-29 15:07:19 +0900

    登録時のデフォルトのカテゴリー追加

--- src/functions/account/lib/account.class.php
+++ src/functions/account/lib/account.class.php
@@ -8,7 +8,7 @@ class Account
         $this->pdo = $db;
     }
 
-    public function regist_account($data)
+    public function regist_account($data)//アカウントの登録
     {
         $data['password'] = $this->pass_hash($data); //パスワードのハッシュ化
         $table = ' Member ';
@@ -22,38 +22,38 @@ class Account
         $this->pdo->insert($table, $column, $value, $pre_value);
         $member_id = $this->get_member_id($data['email']); //valueが違うので明日はこの部分の手直し
         $this->answer_regist($data, $member_id); //秘密の質問登録
+        $this->defaultCategoryInsert($member_id); //各アカウントごとのデフォルトのカテゴリー追加
         return;
     }
-    private function defaultCategoryInsert($member_id)
+    private function defaultCategoryInsert($member_id) //各アカウントごとのデフォルトのカテゴリー追加
     {
-        $table=' Category ';
+        $table = ' Category ';
         $column = 'member_id,c_name ';
         $value = ' :member_id , :c_name ';
         $pre_value = [
             ':member_id' => $member_id,
             ':c_name' => "すべて"
         ];
-        $result = $this->pdo->insert($table, $column, $value, $pre_value);
-
+        $this->pdo->insert($table, $column, $value, $pre_value);
+        return;
     }
 
-    public function get_member_id($email)
+    public function get_member_id($email) //アカウントID取得
     {
         $table = ' Member ';
         $column = ' member_id ';
-        $value = [':email' => "{$email}"];
+        $value = [':email' => $email];
         $option = ' where email=:email';
         $result = $this->pdo->select($table, $column, $value, $option);
-        return $result['member_id'];
+        return $result[0]['member_id'];
     }
 
-    private function pass_hash($data)
+    private function pass_hash($data) //パスワードのハッシュ化
     {
         return password_hash($data['password'], PASSWORD_DEFAULT);
     }
 
-
-    private function answer_regist($data, $member_id)
+    private function answer_regist($data, $member_id) //秘密の質問の答えの登録
     {
         $table = ' Answer ';
         $column = ' question_id, member_id, a_content ';
