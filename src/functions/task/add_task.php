399a702dcf9bdbc067ed87ad6501012fcd07044b
DESKTOP-OF2EO8K\urano <urano@nstg.co.jp>
2020-09-30 11:48:15 +0900

    カテゴリー追加時のタスクのエラー文回避

--- src/functions/task/add_task.php
+++ src/functions/task/add_task.php
@@ -11,19 +11,21 @@ $err = new TaskError();
 
 $member_id = $_SESSION['member_id']; //会員情報
 $data = $_POST;
-unset($data['add']);
 
 if ($_POST['add_category']) { //カテゴリーの追加が行われたとき
     unset($data['add_category']);
     $task->addCategory($data, $member_id); //カテゴリーの追加
     $_SESSION['add_category'] = '新しいカテゴリーが追加されました';
 }
-$errArray = $err->err_check($data);
-if (!empty($errArray)) { //エラーがある場合
-    $_SESSION['err_task'] = $errArray;
-    header('Location:' . './../view/add_task.view.php');
-} else { //成功の場合
-    $task->addTask($data, $member_id); //タスクの新規登録
-    unset($_SESSION['err_task']);
-    header('Location:' . './../view/task_show.view.php');
+if ($data['add']) {//タスクが追加されたとき
+    unset($data['add']);
+    $errArray = $err->err_check($data);
+    if (!empty($errArray)) { //エラーがある場合
+        $_SESSION['err_task'] = $errArray;
+        header('Location:' . './../view/add_task.view.php');
+    } else { //成功の場合
+        $task->addTask($data, $member_id); //タスクの新規登録
+        unset($_SESSION['err_task']);
+        header('Location:' . './../view/task_show.view.php');
+    }
 }
--- src/functions/view/add_task.view.php
+++ src/functions/view/add_task.view.php
@@ -10,90 +10,94 @@ require_once('./../task/show_category.php');
 
 <body>
     <?php require_once('./../common/header.php'); ?>
-    <div class="wrapper add-task-box">
-        <h2>タスク新規登録</h2>
-        <p><span class="red">*</span> 必須項目</p>
-        <form action="./../task/add_task.php" method="post" name="task_form">
-            <div class="grid-form task-form">
-                <div>
-                    <p class="red">
-                        <?php
-                        if ($_SESSION['err_task']) {
-                            echo $_SESSION['err_task']['err_task_name'];
-                        } ?>
-                    </p>
-                    <span class="red">*</span>
-                    <input type="text" name="task_name" placeholder="タスク名"><br>
+    <div class="add-box">
+        <div class="add-task-box">
+            <h2>タスク新規登録</h2>
+            <p><span class="red">*</span> 必須項目</p>
+            <form action="./../task/add_task.php" method="post" name="task_form">
+                <div class="grid-form task-form">
+                    <div>
+                        <p class="red">
+                            <?php
+                            if ($_SESSION['err_task']) {
+                                echo $_SESSION['err_task']['err_task_name'];
+                            } ?>
+                        </p>
+                        <span class="red">*</span>
+                        <label>タスク名</label>
+                        <input type="text" name="task_name"><br>
+                    </div>
+
+                    <div>
+                        <p class="red">
+                            <?php
+                            if ($_SESSION['err_task']) {
+                                echo $_SESSION['err_task']['err_priority'];
+                            } ?>
+                        </p>
+                        <span class="red">*</span>
+                        <label>優先順位</label>
+                        <select name="priority">
+                            <option value="">未選択</option>
+                            <option value="1">1</option>
+                            <option value="2">2</option>
+                            <option value="3">3</option>
+                            <option value="4">4</option>
+                            <option value="5">5</option>
+                        </select>
+                    </div>
+                    <div>
+                        <label>カテゴリー</label>
+                        <select name="category_id">
+                            <?php
+                            for ($i = 0; $i < count($category_list); $i++) {
+                            ?>
+                                <option value="<?php echo $category_list[$i]['category_id'] ?>"><?php echo $category_list[$i]['c_name']; ?></option>
+                            <?php } ?>
+                        </select>
+                    </div>
+                    <div>
+                        <span>メモ</span>
+                        <input type="text" name="comment"><br>
+                    </div>
+                    <div>
+                        <label>開始
+                            <input type="date" name="start_date" value='null' placeholder="始める日"><br>
+                        </label>
+                    </div>
+                    <div>
+                        <label>終了
+                            <input type="date" name="limit_date" value='null' placeholder="終わる日"><br>
+                        </label>
+                    </div>
                 </div>
-                <div></div>
-                <div>
-                    <p class="red">
-                        <?php
-                        if ($_SESSION['err_task']) {
-                            echo $_SESSION['err_task']['err_priority'];
-                        } ?>
-                    </p>
-                    <span class="red">*</span>
-                    <label>優先順位</label>
-                    <select name="priority">
-                        <option value="">未選択</option>
-                        <option value="1">1</option>
-                        <option value="2">2</option>
-                        <option value="3">3</option>
-                        <option value="4">4</option>
-                        <option value="5">5</option>
-                    </select>
+                <div class="center">
+                    <input type="submit" class="button" name="add" value="登録">
                 </div>
-                <div></div>
-                <div>
-                    <p class="red">
-                        <?php
-                        if ($_SESSION['err_task']) {
-                            echo $_SESSION['err_task']['err_category'];
-                        } ?>
-                    </p>
-                    <span class="red">*</span>
-                    <label>カテゴリー</label>
-                    <select name="category_id">
-                        <option value="">未選択</option>
-                        <?php
-                        for ($i = 0; $i < count($category_list); $i++) {
-                        ?>
-                            <option value="<?php echo $category_list[$i]['category_id'] ?>"><?php echo $category_list[$i]['c_name']; ?></option>
-                        <?php } ?>
-                    </select>
+            </form>
+        </div>
+
+        <!--カテゴリー追加-->
+        <div class="edit-box">
+            <form action="./../task/add_task.php" method="post">
+                <div class="new-task">
+                    <h2>カテゴリーの追加</h2>
+                    <div class="edit-form task-form">
+                        <p class="red">
+                            <?php
+                            if ($_SESSION['add_category']) {
+                                echo $_SESSION['add_category'];
+                            } ?>
+                        </p>
+                        <input type="text" name="c_name" placeholder="カテゴリーの追加">
+                        <input type="submit" name="add_category" value="追加">
+                    </div>
                 </div>
-                <div>
-                    <p class="red">
-                        <?php
-                        if ($_SESSION['add_category']) {
-                            echo $_SESSION['add_category'];
-                        } ?>
-                    </p>
-                    <input type="text" name="c_name" placeholder="カテゴリーの追加">
-                    <input type="submit" name="add_category" value="追加">
-                </div>
-                <div>
-                    <span>メモ</span>
-                    <input type="text" name="comment"><br>
-                </div>
-                <div></div>
-                <div>
-                    <label>開始
-                        <input type="date" name="start_date" value='null' placeholder="始める日"><br>
-                    </label>
-                </div>
-                <div>
-                    <label>終了
-                        <input type="date" name="limit_date" value='null' placeholder="終わる日"><br>
-                    </label>
-                </div>
-            </div>
-            <div class="center">
-                <input type="submit" class="button" name="add" value="登録">
-            </div>
-        </form>
+            </form>
+            <!-- edit-box -->
+        </div>
     </div>
+    <!--  -->
     <?php require_once('./../common/footer.php'); ?>
 </body>
 
